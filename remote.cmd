893b8661edfd2196c36273fb946990b97f7c34e6
nexora-kz <zhanat.serikov@gmail.com>
2026-09-17 08:45:50 +0500

    Pin Windows bootstrap to tested isolated-cache commit

--- remote.cmd
+++ remote.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "$u='https://raw.githubusercontent.com/nexora-kz/NEXORA-Hands/main/remote.ps1?cb='+[guid]::NewGuid().ToString('N'); irm $u | iex"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/nexora-kz/NEXORA-Hands/38dbef3dcec1f8edc33008b90851a1acb00ff62e/remote.ps1 | iex"
 if errorlevel 1 (
   echo.
   echo NEXORA Hands failed to start. Press any key to close.
