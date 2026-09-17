78208bc42fdcf15dbbf6c7e43070a20f9360271d
NEXORA <zhanat.serikov@gmail.com>
2026-09-18 00:28:18 +0500

    Release background process fix

--- remote.ps1
+++ remote.ps1
@@ -36,7 +36,7 @@ try {
     $dataRoot = Join-Path $runtimeRoot 'data'
     $appRoot = Join-Path $runtimeRoot 'app'
     New-Item -ItemType Directory -Force -Path $dataRoot,$appRoot | Out-Null
-    $rawBase = 'https://raw.githubusercontent.com/nexora-kz/NEXORA-Hands/b8d03a1'
+    $rawBase = 'https://raw.githubusercontent.com/nexora-kz/NEXORA-Hands/95ffa1c'
     $files = @(
         @{Url="$rawBase/start.ps1";Path=(Join-Path $runtimeRoot 'start.ps1')},
         @{Url="$rawBase/stop.ps1";Path=(Join-Path $runtimeRoot 'stop.ps1')},
