d830fc2f27b1f70e5d7589278a8f9f2d19289234
NEXORA <zhanat.serikov@gmail.com>
2026-09-17 22:30:40 +0500

    Pin launcher runtime to worker identity fix

--- remote.ps1
+++ remote.ps1
@@ -36,7 +36,7 @@ try {
     $dataRoot = Join-Path $runtimeRoot 'data'
     $appRoot = Join-Path $runtimeRoot 'app'
     New-Item -ItemType Directory -Force -Path $dataRoot,$appRoot | Out-Null
-    $rawBase = 'https://raw.githubusercontent.com/nexora-kz/NEXORA-Hands/e87d40e'
+    $rawBase = 'https://raw.githubusercontent.com/nexora-kz/NEXORA-Hands/48feef7'
     $files = @(
         @{Url="$rawBase/start.ps1";Path=(Join-Path $runtimeRoot 'start.ps1')},
         @{Url="$rawBase/stop.ps1";Path=(Join-Path $runtimeRoot 'stop.ps1')},
