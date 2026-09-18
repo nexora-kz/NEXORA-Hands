62d9e79372640d0c7bbfdae10ce15e7b15838a64
NEXORA <zhanat.serikov@gmail.com>
2026-09-18 08:10:47 +0500

    Pin launcher to shell reliability update

--- remote.ps1
+++ remote.ps1
@@ -44,7 +44,7 @@ try {
     $dataRoot = Join-Path $runtimeRoot 'data'
     $appRoot = Join-Path $runtimeRoot 'app'
     New-Item -ItemType Directory -Force -Path $dataRoot,$appRoot | Out-Null
-    $rawBase = 'https://raw.githubusercontent.com/nexora-kz/NEXORA-Hands/d117fd5'
+    $rawBase = 'https://raw.githubusercontent.com/nexora-kz/NEXORA-Hands/f5e478c'
     $files = @(
         @{Url="$rawBase/start.ps1";Path=(Join-Path $runtimeRoot 'start.ps1')},
         @{Url="$rawBase/stop.ps1";Path=(Join-Path $runtimeRoot 'stop.ps1')},
