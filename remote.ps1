69d0da27b55c30a793a298b501ce51ec896ca838
NEXORA <zhanat.serikov@gmail.com>
2026-09-18 08:27:26 +0500

    Pin launcher to rich terminal runtime

--- remote.ps1
+++ remote.ps1
@@ -44,7 +44,7 @@ try {
     $dataRoot = Join-Path $runtimeRoot 'data'
     $appRoot = Join-Path $runtimeRoot 'app'
     New-Item -ItemType Directory -Force -Path $dataRoot,$appRoot | Out-Null
-    $rawBase = 'https://raw.githubusercontent.com/nexora-kz/NEXORA-Hands/089980f'
+    $rawBase = 'https://raw.githubusercontent.com/nexora-kz/NEXORA-Hands/b62a1a1'
     $files = @(
         @{Url="$rawBase/start.ps1";Path=(Join-Path $runtimeRoot 'start.ps1')},
         @{Url="$rawBase/stop.ps1";Path=(Join-Path $runtimeRoot 'stop.ps1')},
