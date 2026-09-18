1571466fc6cb9c4728b2dbdea971a838cd03217a
NEXORA <zhanat.serikov@gmail.com>
2026-09-18 07:06:30 +0500

    Pin launcher to health-aware runtime

--- remote.ps1
+++ remote.ps1
@@ -44,7 +44,7 @@ try {
     $dataRoot = Join-Path $runtimeRoot 'data'
     $appRoot = Join-Path $runtimeRoot 'app'
     New-Item -ItemType Directory -Force -Path $dataRoot,$appRoot | Out-Null
-    $rawBase = 'https://raw.githubusercontent.com/nexora-kz/NEXORA-Hands/66092da'
+    $rawBase = 'https://raw.githubusercontent.com/nexora-kz/NEXORA-Hands/e16cf43'
     $files = @(
         @{Url="$rawBase/start.ps1";Path=(Join-Path $runtimeRoot 'start.ps1')},
         @{Url="$rawBase/stop.ps1";Path=(Join-Path $runtimeRoot 'stop.ps1')},
